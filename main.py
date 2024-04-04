d148c4a8ed249c406e24dc9c302a99b5217b0451
Bondarev Artem <bond210484@mail.ru>
2024-04-04 10:53:45 +0700

    add task №3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pprint
+from pprint import pprint
 def my_cookbook():
     with open('recipes.txt', encoding='utf-8') as file:
         cook_book = {}
@@ -10,7 +10,18 @@ def my_cookbook():
                 tmp.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
             cook_book[name] = tmp
     return cook_book
-pprint.pprint(my_cookbook())
+#pprint.pprint(my_cookbook())
 
 def get_shop_list_by_dishes(dishes, person_count):
-    
+    menu_dishes = {}
+    for dish in dishes:
+        if dish in my_cookbook():
+            tmp = my_cookbook().get(dish)
+            for i in tmp:
+                if i['ingredient_name'] in menu_dishes:
+                    menu_dishes[i['ingredient_name']]['quantity'] += i['quantity'] * person_count
+                else:
+                    menu_dishes[i['ingredient_name']] = {'measure': i['measure'], 'quantity': i['quantity'] * person_count}
+    return menu_dishes
+#pprint(get_shop_list_by_dishes(["Омлет","Фахитос"],2))
+
